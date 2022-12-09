d46fdbffaa29c431a709ee8dd69c0fad45aa3d70
Adam Labiosa <labiosa@wisc.edu>
2022-12-09 15:54:43 -0600

    -

--- tianshou/trainer/base.py
+++ tianshou/trainer/base.py
@@ -290,7 +290,6 @@ class BaseTrainer(ABC):
             print(self.stop_fn_flag)
             if self.distributed:
                 stop = self.stop_fn_flag or self.epoch > self.max_epoch
-                dist.stop_server()
                 # Wait for all processes to finish
                 print('at barrier')
                 # Gather and scatter self.stop_fn_flag 
