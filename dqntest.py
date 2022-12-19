a64c482099fd658c4c416518550ae9ef6ef5eaa9
Adam Labiosa <labiosa@wisc.edu>
2022-12-18 22:31:17 -0600

    -

--- dqntest.py
+++ dqntest.py
@@ -32,27 +32,32 @@ test_envs = gym.make('CartPole-v0')
 train_envs = ts.env.DummyVectorEnv([lambda: gym.make('CartPole-v0') for _ in range(10)])
 test_envs = ts.env.DummyVectorEnv([lambda: gym.make('CartPole-v0') for _ in range(100)])
 
-class Net(nn.Module):
-    def __init__(self, state_shape, action_shape):
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(np.prod(state_shape), 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, np.prod(action_shape)),
-        )
+# class Net(nn.Module):
+#     def __init__(self, state_shape, action_shape):
+#         super().__init__()
+#         self.model = nn.Sequential(
+#             nn.Linear(np.prod(state_shape), 128), nn.ReLU(inplace=True),
+#             nn.Linear(128, 128), nn.ReLU(inplace=True),
+#             nn.Linear(128, 128), nn.ReLU(inplace=True),
+#             nn.Linear(128, np.prod(action_shape)),
+#         )
 
-    def forward(self, obs, state=None, info={}):
-        if not isinstance(obs, torch.Tensor):
-            obs = torch.tensor(obs, dtype=torch.float)
-        batch = obs.shape[0]
-        logits = self.model(obs.view(batch, -1))
-        return logits, state
+#     def forward(self, obs, state=None, info={}):
+#         if not isinstance(obs, torch.Tensor):
+#             obs = torch.tensor(obs, dtype=torch.float)
+#         batch = obs.shape[0]
+#         logits = self.model(obs.view(batch, -1))
+#         return logits, state
 
 state_shape = env.observation_space.shape or env.observation_space.n
 action_shape = env.action_space.shape or env.action_space.n
-net = Net(state_shape, action_shape)
+# Creat branching network
+net = ts.network.BranchingDQNNet(state_shape, action_shape, 2, 2)
+
+# net = Net(state_shape, action_shape)
 optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+
 
 # policy = ts.policy.DQNPolicy(net, optim, discount_factor=0.9, estimation_step=3, target_update_freq=320, distr=DISTRIBUTED, num_nodes=4, rank=rank)
 policy = ts.policy.BranchingDQNPolicy(net, optim, discount_factor=0.9, target_update_freq=320, distr=DISTRIBUTED, num_nodes=4, rank=rank)
