37177d7c39ccd73fa045ad51f33100e24b81a28a
Adam Labiosa <labiosa@wisc.edu>
2022-12-18 21:12:52 -0600

    -

--- tianshou/policy/modelfree/ppo.py
+++ tianshou/policy/modelfree/ppo.py
@@ -137,6 +137,7 @@ class PPOPolicy(A2CPolicy):
         for step in range(repeat):
             if self._recompute_adv and step > 0:
                 batch = self._compute_returns(batch, self._buffer, self._indices)
+            sync = 0
             for minibatch in batch.split(batch_size, merge_last=True):
                 # calculate loss for actor
                 dist = self(minibatch).dist
@@ -181,12 +182,13 @@ class PPOPolicy(A2CPolicy):
                 if self.distr:
                     # distribute.barrier()
                     # every 20 iterations, sync the gradients
-                    if step % 50000 == 0:
+                    if sync % 20 == 0:
                         print('syncing gradients')
                         # sync gradients
                         for param in self._actor_critic.parameters():
                             distribute.all_reduce(param.grad.data, op=distribute.ReduceOp.SUM)
                             param.grad.data /= distribute.get_world_size()
+                    sync += 1
 
 
                 self.optim.step()
