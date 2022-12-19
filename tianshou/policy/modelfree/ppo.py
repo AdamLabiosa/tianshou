5a98e014647d52fdc6507b34a8d6c043da4637a0
Adam Labiosa <labiosa@wisc.edu>
2022-12-18 19:44:52 -0600

    -

--- tianshou/policy/modelfree/ppo.py
+++ tianshou/policy/modelfree/ppo.py
@@ -183,7 +183,7 @@ class PPOPolicy(A2CPolicy):
                     # every 20 iterations, sync the gradients
                     if step % 20 == 0:
                         # sync gradients
-                        for param in self.model.parameters():
+                        for param in self._actor_critic.parameters():
                             distribute.all_reduce(param.grad.data, op=distribute.ReduceOp.SUM)
                             param.grad.data /= distribute.get_world_size()
 
